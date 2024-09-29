5be26fa7b9d0dcb9ce8106b8bfddcf5b6d00fb77
Dmitry <dmitryryzhenkov.dev@gmail.com>
2024-09-29 22:59:09 +0200

    setup gui done

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+import json
+
+from typing import TypeVar
+
+T = TypeVar("T", bound="Config")
+
+class Config:
+    _instance: "Config" = None
+
+    def __new__(cls, *args, **kwargs) -> T:
+        if cls._instance is None:
+            cls._instance = super(Config, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def __init__(self) -> None:
+        # TODO: process initialization logic
+        
+        self.HOME = os.getenv("HOME")
+        self.backend_api_url = "https://www.google.com"
+        self.sign_up_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        self._default_kvks_dir = f"{self.HOME}\\FPSAimTrainer"
+        self.credentials_path = f"{self.HOME}\\.kvkstracker\\credentials.json"
+        self.config_path = f"{self.HOME}\\.kvkstracker\\config.json"
+
+        try:
+            os.mkdir(f"{self.HOME}\\.kvkstracker")
+        except FileExistsError: pass
+
+        try:
+            with open(self.credentials_path) as file:
+                data = json.load(file)
+                self.username = data.get("username")
+                self.password = data.get("password")
+        except FileNotFoundError:
+            self.username = None
+            self.password = None
+
+        try:
+            with open(self.config_path) as file:
+                data = json.load(file)
+                self.kvks_dir = data.get("kvks_dir", self._default_kvks_dir)
+        except FileNotFoundError:
+            self.kvks_dir = self._default_kvks_dir
+
+    def cache(self):
+        print("cache", "w")
+        with open(self.credentials_path, "w") as file:
+            json.dump({"username" : self.username, "password" : self.username}, file)
+        
+        with open(self.config_path, "w") as file:
+            json.dump({"kvks_dir" : self.kvks_dir}, file)
---
+++ setup/gui.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import filedialog
+
+import webbrowser
+import os
+
+import requests
+
+import config
+
+class Setup(tk.Tk):
+
+    def __init__(self):
+        print("Setup __init__")
+        super().__init__()
+
+        self.title("KovaaK's - Voltaic Tracker Setup")
+        self.geometry("600x300")
+        self.resizable(False, False)
+
+        self.config = config.Config()
+
+        # Authentication GUI and logic elements
+
+        self._username = tk.StringVar(self, self.config.username)
+        self._password = tk.StringVar(self, self.config.password)
+
+        self.username_label = tk.Label(self, text="Username")
+        self.username_entry = tk.Entry(self, textvariable=self._username)
+        self.password_label = tk.Label(self, text="Password")
+        self.password_entry = tk.Entry(self, textvariable=self._password, show="*")
+
+        self.sign_up_label = tk.Label(self, text="Don't have an account?")
+        self.sign_up_button = tk.Button(self, text="Sign Up", command=self._sign_up)
+
+        # Authentication error messages
+        self.invalid_credentials_label = tk.Label(self, text="Invalid credentials")
+
+        # KovaaK's config GUI and logic elements
+
+        self._kvks_dir = tk.StringVar(self, self.config.kvks_dir)
+
+        self.kvks_dir_label = tk.Label(self, text="KovaaK's directory")
+        self.kvks_dir_entry = tk.Entry(self, textvariable=self._kvks_dir)
+        self.browse_button = tk.Button(self, text="Browse", command=self._browse)
+        self.confirm_button = tk.Button(self, text="Done!", command=self._wrap_up)
+
+        # KovaaK's config error messages
+        self.dir_not_found_label = tk.Label(self, text="Directory not found")
+
+        self._authenticate()  
+        self._config()
+            
+
+    def _authenticate(self):
+
+        self.username_label.pack(pady=(15,0))
+        self.username_entry.pack()
+
+        self.password_label.pack()
+        self.password_entry.pack()
+
+        self.sign_up_label.pack(pady=(15,0))
+        self.sign_up_button.pack(pady=(5,0))
+
+    def _config(self):
+        print("Setup _config")
+        
+        self.kvks_dir_label.pack(pady=(20,0))
+        self.kvks_dir_entry.pack()
+        self.browse_button.pack(pady=(5,0))
+        self.confirm_button.pack(pady=(10,0))
+
+
+    def _sign_in(self):
+        
+        self._username.set(self.username_entry.get())
+        self._password.set(self.password_entry.get())
+        self.config.username = self._username.get()
+        self.config.password = self._password.get()
+        self.config.cache()
+
+        response = requests.get(self.config.backend_api_url)
+        if not response.status_code == 200:
+            self.invalid_credentials_label.pack()
+
+    def _sign_up(self):
+        webbrowser.open(self.config.sign_up_url)
+
+    def _browse(self):
+        directory = filedialog.askdirectory()
+        self._kvks_dir.set(directory)
+
+    def _wrap_up(self):
+        if os.path.exists(self._kvks_dir.get()):
+            self.config.kvks_dir = self._kvks_dir.get()
+            self._sign_in()
+            self.config.cache()
+            self.quit()
+        else:
+            self.dir_not_found_label.pack()
+
+def main():
+    app = Setup()    
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
